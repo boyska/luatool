dab343bb1cd74bbcadb376333758a480bb2767db
boyska <piuttosto@logorroici.org>
2015-08-10 03:08:33 +0200

    remove subcommand, better connection handling
    telnet connection is now smoother, and writeln check if the command has
    been executed multiple times, trying to avoid both failures and slowness

--- luatool/luatool.py
+++ luatool/luatool.py
@@ -27,10 +27,11 @@ import re
 
 version = "0.6.3"
 
+
 def get_file_list():
     filedesc = re.compile(r'^name:([^, ]*), size:([0-9]+)$')
     data = writeln("local l = file.list();for k,v in pairs(l) do "
-            "  print('name:'..k..', size:'..v)end\n", True)
+                   "print('name:'..k..', size:'..v) end\n", True)
     found = {}
     for l in data.split('\n'):
         line = l.strip()
@@ -85,27 +86,35 @@ def serial_writer(data, check=True):
         return None
 writeln = serial_writer
 
-def telnet_writer(data, check=True):
+
+def telnet_writer(data, ignored=True):
+    if ignored is not True:
+        import warnings
+        warnings.warn("check attribute for telnet_writer is ignored",
+                      warnings.DeprecationWarning)
     kind_of_newlines = re.compile(r'\n|\r')
     logging.debug("Sent [[ {} ]]".format(data))
     s.write(data)
-    sleep(0.3)
-    if not check:
-        sys.stdout.write(" -> send without check")
-
-    response = s.read_until('>')
-    logging.debug("Received [[ {} ]]".format(response))
-    if not '>' in response:
-        raise Exception('> not found')
+    # we need to wait for ">", but "read_until" seems not reliable enough
+    # so we just do some attemps, sleeping always a bit more
+    for i in xrange(7):
+        sleep(i * 0.1)  # at every attempt, we sleep something more
+        response = s.read_until('>', 4)
+        logging.debug("Received [[ {} ]]".format(response))
+        if '>' in response:
+            # ok, we have reached it
+            break
+    else:
+        raise Exception('Error in response: > not found')
     response = response[:response.index('>')]
     lines = kind_of_newlines.split(response)
 
     if any((line.strip().startswith('lua:') for line in lines)):
-        logging.error("Received lua errors\n%s" % '\n'.join((line for line in
-            lines if line.strip().startswith('lua:'))))
+        logging.error("Received lua errors\n%s" %
+                      '\n'.join((line for line in lines if
+                                 line.strip().startswith('lua:'))))
         raise Exception('ERROR from Lua interpreter\r\n\r\n')
     return response
-
 
 
 def writer(data):
@@ -115,7 +124,13 @@ def writer(data):
 def openserial(args):
     if args.telnet:
         import telnetlib
-        s = telnetlib.Telnet(args.telnet, args.telnet_port, timeout=20)
+        logging.debug("connecting")
+        s = telnetlib.Telnet(args.telnet, args.telnet_port, timeout=10)
+        logging.debug("waiting for prompt")
+        sleep(1)
+        banner = s.read_very_eager()
+        logging.debug("Banner '%s'" % banner)
+        logging.info("connection ready")
         return s
     else:
         import serial
@@ -124,11 +139,7 @@ def openserial(args):
             s = serial.Serial(args.port, args.baud)
         except:
             raise Exception("Could not open port %s\n" % (args.port))
-        if args.verbose:
-            sys.stderr.write("Set timeout %s\r\n" % s.timeout)
         s.timeout = 3
-        if args.verbose:
-            sys.stderr.write("Set interCharTimeout %s\r\n" % s.interCharTimeout)
         s.interCharTimeout = 3
         return s
 
@@ -146,9 +157,13 @@ def get_parser():
     cmd_list.set_defaults(func=main_list)
     cmd_wipe = sub.add_parser('wipe')
     cmd_wipe.set_defaults(func=main_wipe)
+    cmd_remove = sub.add_parser('remove')
+    cmd_remove.set_defaults(func=main_remove)
+    cmd_remove.add_argument('fname', metavar='FILENAME')
     cmd_upload = sub.add_parser('upload')
     cmd_upload.set_defaults(func=main_upload)
-    cmd_upload.add_argument('src', metavar='FILENAME')
+    cmd_upload.add_argument('src', metavar='FILENAME',
+                            type=argparse.FileType('r'))
     cmd_upload.add_argument('-t', '--dest')
     cmd_upload.add_argument('-c', '--compile', action='store_true',    help='Compile lua to lc after upload')
     cmd_upload.add_argument('-a', '--append',  action='store_true',    help='Append source file to destination file.')
@@ -162,74 +177,62 @@ def main_list(args):
     for filename in flist:
         print '%(name)s\t(size=%(size)d)' % flist[filename]
 
+
 def main_wipe(args):
     for fn in get_file_list():
         logging.debug("Delete file {} from device.\r\n".format(fn))
         writeln("file.remove(\"" + fn + "\")\r")
 
+
+def main_remove(args):
+    writeln("file.remove(\"" + args.fname + "\")\r")
+
+
 def main_upload(args):
     if args.dest is None:
-        args.dest = basename(args.src)
-
-    # open source file for reading
-    try:
-        f = open(args.src, "rt")
-    except:
-        sys.stderr.write("Could not open input file \"%s\"\n" % args.src)
-        return 1
+        args.dest = basename(args.src.name)
 
     # Verify the selected file will not exceed the size of the serial buffer.
     # The size of the buffer is 256. This script does not accept files with
     # lines longer than 230 characters to have some room for command overhead.
-    for ln in f:
+    for ln in args.src:
         if len(ln) > 230:
             sys.stderr.write("File \"%s\" contains a line with more than 240 "
                              "characters. This exceeds the size of the serial buffer.\n"
                              % args.src)
-            f.close()
+            args.src.close()
             return 1
 
     # Go back to the beginning of the file after verifying it has the correct
     # line length
-    f.seek(0)
-
-    # Open the selected serial port
-    s = openserial(args)
+    args.src.seek(0)
 
     # set serial timeout
     if args.verbose:
         sys.stderr.write("Upload starting\r\n")
 
     # remove existing file on device
-    if args.append==False:
-        if args.verbose:
-            sys.stderr.write("Stage 1. Deleting old file from flash memory")
+    if args.append is not True:
+        logging.info("Stage 1. Deleting old file from flash memory")
         writeln("file.open(\"" + args.dest + "\", \"w\")\r")
         writeln("file.close()\r")
         writeln("file.remove(\"" + args.dest + "\")\r")
     else:
-        if args.verbose:
-            sys.stderr.write("[SKIPPED] Stage 1. Deleting old file from flash memory [SKIPPED]")
-
+        logging.info("[SKIPPED] Stage 1. Deleting old file from flash memory [SKIPPED]")
 
     # read source file line by line and write to device
-    if args.verbose:
-        sys.stderr.write("\r\nStage 2. Creating file in flash memory and write first line")
-    if args.append: 
+    logging.info("\r\nStage 2. Creating file in flash memory and write first line")
+    if args.append:
         writeln("file.open(\"" + args.dest + "\", \"a+\")\r")
     else:
         writeln("file.open(\"" + args.dest + "\", \"w+\")\r")
-    line = f.readline()
-    if args.verbose:
-        sys.stderr.write("\r\nStage 3. Start writing data to flash memory...")
-    while line != '':
+    logging.info("\r\nStage 3. Start writing data to flash memory...")
+    for line in args.src:
         writer(line.strip())
-        line = f.readline()
 
     # close both files
-    f.close()
-    if args.verbose:
-        sys.stderr.write("\r\nStage 4. Flush data and closing file")
+    args.src.close()
+    logging.info("\r\nStage 4. Flush data and closing file")
     writeln("file.flush()\r")
     writeln("file.close()\r")
 
@@ -273,5 +276,3 @@ if __name__ == '__main__':
     s = openserial(args)
     ret = args.func(args)
     sys.exit(0 if ret is None else ret)
-
-
